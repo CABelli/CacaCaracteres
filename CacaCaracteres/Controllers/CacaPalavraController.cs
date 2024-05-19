f6a4e0fc24fe67de01f7fce1e083bac2003b4c58
Cesar Antonio Belli <cesar.belli@ambevtech.com.br>
2024-05-19 10:35:12 -0300

    Add project files.

---
+++ CacaCaracteres/Controllers/CacaPalavraController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CacaCaracteres.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class CacaPalavraController : ControllerBase
+{
+    [HttpGet(Name = "Get-Ok")]
+    public Saida Get()
+    {
+        var saida = new Saida { Resultado = "Foi" };
+        return saida;
+    }
+}
+public class Saida
+{
+    public string? Resultado { get; set; }
+}
