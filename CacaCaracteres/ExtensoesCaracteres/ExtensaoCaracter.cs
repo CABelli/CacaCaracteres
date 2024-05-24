40226d73cd1759af60d27f38669004b1e99d2d96
Cesar Antonio Belli <cesar.belli@ambevtech.com.br>
2024-05-24 13:34:55 -0300

    Ajuste 02 classe de extensao de caracteres

--- CacaCaracteres/ExtensoesCaracteres/ExtensaoCaracter.cs
+++ CacaCaracteres/ExtensoesCaracteres/ExtensaoCaracter.cs
@@ -4,14 +4,20 @@
     {
         public static char? RetornaLetra(this char caracter)
         {
-            if ((caracter >= 'a' && caracter <= 'z') || 
-                (caracter >= 'A' && caracter <= 'Z')) return caracter;
+            if (caracter >= 'a' && caracter <= 'z') return caracter;
             return null;            
         }
 
         public static char? RetornaVogal(this char caracter)
         {
-            if ( "aeiouAEIOU".Contains(caracter)) return caracter;
+            if ("aeiou".Contains(caracter)) return caracter;
+            return null;
+        }
+
+        public static char? RetornaConsoante(this char caracter)
+        {
+            if ( RetornaLetra(caracter) == caracter && 
+                RetornaVogal(caracter) == null ) return caracter;
             return null;
         }
     }
--- CacaCaracteres/Servicos/ResumoTextoServico.cs
+++ CacaCaracteres/Servicos/ResumoTextoServico.cs
@@ -1,6 +1,8 @@
 ﻿using CacaCaracteres.Dto;
 using CacaCaracteres.ExtensoesCaracteres;
 using CacaCaracteres.Interfaces;
+using System.Text;
+using static System.Net.Mime.MediaTypeNames;
 
 namespace CacaCaracteres.Servicos
 {
@@ -16,12 +18,18 @@ namespace CacaCaracteres.Servicos
                 };
                 return saidaVazia;
             }
+
+            var entradaNormalizada = entrada.Texto.ToLower().Normalize(NormalizationForm.FormD);
+
             var saida = new SaidaCacaPalavrasDto
             {
                 Texto = entrada.Texto,
-                NumeroDePalavras = entrada.Texto.Split(' ').Length,
-                NumeroDeLetras = entrada.Texto.Count( x => x == x.RetornaLetra()),
-                NumeroDeVogais = entrada.Texto.Count( x => x == x.RetornaVogal())
+                NumeroDePalavras = entradaNormalizada.Split(' ').Length,
+                NumeroDeLetras = entradaNormalizada.Count( x => x == x.RetornaLetra()),
+                NumeroDeVogais = entradaNormalizada.Count( x => x == x.RetornaVogal()),
+                NumeroDeConsonantes = entradaNormalizada.Count(x => x == x.RetornaConsoante()),
+                NumeroDeMaisculas = entrada.Texto.Count(char.IsUpper),
+                NumeroDeMinuscolas = entrada.Texto.Count( char.IsLower)
             };
             return saida;
         }
